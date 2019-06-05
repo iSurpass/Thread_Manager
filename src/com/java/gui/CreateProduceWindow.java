b2eb07239ac66f0e5cb11d69448c4874e6754a2b
iSurpass <798051216@qq.com>
2019-06-05 15:19:25 +0800

    up03

--- src/com/java/gui/CreateProduceWindow.java
+++ src/com/java/gui/CreateProduceWindow.java
@@ -26,6 +26,8 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
 
     public static Object[] row = new Object[4];
 
+    public static boolean flag = false;
+
     public CreateProduceWindow(){
         this.setSize(400,400);
         this.setVisible(true);
@@ -58,7 +60,12 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (e.getSource() == b1){
+            JOptionPane.showMessageDialog(null,"已创建PID为"+pid+"的进程");
+            /*int res=JOptionPane.showConfirmDialog(null, "已创建进程1", "信息", JOptionPane.YES_OPTION);
+            if(res==JOptionPane.YES_OPTION){
+            }*/
             System.out.println("234234");
             String name = textField.getText();
             String runTime = textField1.getText();
@@ -70,9 +77,11 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
             produce.setTimeSlice(20);
             produce.setTimeRest(runTime);
 
+            //将新创建的进程put进map中
             produces.add(produce);
             produceMap.put(produce.getPID(),produce);
 
+            //新创建的进程预先加入到就绪队列中
             row[0] = produce.getPID();
             row[1] = produce.getName();
             row[2] = produce.getTimeRest();
@@ -80,8 +89,12 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
             int index = model2.getDataVector().indexOf(row);
             index++;
             System.out.println(index);
+
+            //如果运行队列的进程数小于1时，方可进入队列
             if (model1.getRowCount() < 1){
                 model2.removeRow(index);
+                row[0] = produce.getPID();
+                row[1] = produce.getName();
                 row[2] = produce.getTimeSlice();
                 row[3] = produce.getTimeRest();
                 try {
@@ -95,9 +108,9 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
                 {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        int index = model1.getDataVector().indexOf(row);
-                        index++;
-                        model1.removeRow(index);
+                        //int index = model1.getDataVector().indexOf(row);
+                        //index++;
+                        model1.removeRow(0);
                         row[0] = produce.getPID();
                         row[1] = produce.getName();
                         row[2] = produce.getTimeSlice();
@@ -105,11 +118,16 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
                         model1.addRow(row);
                         int time = Integer.parseInt(produce.getTimeRest());
                         int silce = produce.getTimeSlice();
+                        time -= 5;
+                        if (time < 0){
+                            model1.removeRow(0);
+                        }
                         silce -= 5;
-                        if (silce == 0){
-                            model1.getDataVector().removeElementAt(index);
+                        if (silce == -5){
+                            flag = true;
+                            model1.removeRow(0);
+                            //model1.getDataVector().removeElementAt(index);
                             row[2] = produce.getTimeRest();
-                            model2.addRow(row);
                             silce = 20;
                             int nextIndex = (int) model2.getValueAt(0,0);
                             Produce produceNext = produceMap.get(nextIndex);
@@ -118,19 +136,18 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
                             rowNext[1] = produceNext.getName();
                             rowNext[2] = produceNext.getTimeSlice();
                             rowNext[3] = produceNext.getTimeRest();
-                            model2.getDataVector().removeElementAt(nextIndex);
+                            model2.removeRow(0);
+                            //model2.getDataVector().removeElementAt(nextIndex);
                             model1.addRow(rowNext);
-                            //new Timer(delay,taskPerformer).start();
-                        }
-                        time -= 5;
-                        if (time < 0){
-                            model1.removeRow(0);
+                            model2.addRow(row);
+                            recycle(produceNext);
                         }
                         produce.setTimeRest(String.valueOf(time));
                         produce.setTimeSlice(silce);
                     }
                 };
-                new Timer(delay,taskPerformer).start();
+                Timer timer = new Timer(delay,taskPerformer);
+                timer.start();
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e1) {
@@ -144,7 +161,12 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
                         model1.getDataVector().remove(row);
                     }
                 };
-                new Timer(delay1,taskPerformer1).start();
+                Timer timer1 = new Timer(delay1,taskPerformer1);
+                timer1.start();
+                if (flag){
+                    timer.stop();
+                    timer1.stop();
+                }
             }
             this.setVisible(false);
         }
@@ -152,4 +174,77 @@ public class CreateProduceWindow extends JFrame implements ActionListener {
             this.setVisible(false);
         }
     }
-}
+
+    public void recycle(Produce produce){
+            int i = 0;
+            int delay=5000;    //时间间隔，单位为毫
+            ActionListener taskPerformer=new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //int index = model1.getDataVector().indexOf(row);
+                    //index++;
+                    System.out.println("3333"+model1.getDataVector().get(0));
+                    //model1.getDataVector().remove(row);
+                    model1.removeRow(0);
+                    row[0] = produce.getPID();
+                    row[1] = produce.getName();
+                    row[2] = produce.getTimeSlice();
+                    row[3] = produce.getTimeRest();
+                    model1.addRow(row);
+                    int time = Integer.parseInt(produce.getTimeRest());
+                    int silce = produce.getTimeSlice();
+                    time -= 5;
+                    if (time < 0){
+                        model1.removeRow(0);
+                    }
+                    silce -= 5;
+                    if (silce == -5){
+                        flag = true;
+                        model1.removeRow(0);
+                        //model1.getDataVector().removeElementAt(index);
+                        row[2] = produce.getTimeRest();
+                        silce = 20;
+                        int nextIndex = (int) model2.getValueAt(0,0);
+                        Produce produceNext = produceMap.get(nextIndex);
+                        Object[] rowNext = new Object[4];
+                        rowNext[0] = produceNext.getPID();
+                        rowNext[1] = produceNext.getName();
+                        rowNext[2] = produceNext.getTimeSlice();
+                        rowNext[3] = produceNext.getTimeRest();
+                        model2.removeRow(0);
+                        //model2.getDataVector().removeElementAt(nextIndex);
+                        model1.addRow(rowNext);
+                        model2.addRow(row);
+                        recycle(produceNext);
+                        //new Timer(delay,taskPerformer).start();
+                    }
+                    produce.setTimeRest(String.valueOf(time));
+                    produce.setTimeSlice(silce);
+                }
+            };
+            Timer timer = new Timer(delay,taskPerformer);
+            timer.start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            int delay1=1000;    //时间间隔，单位为毫秒
+            ActionListener taskPerformer1=new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //model1.removeRow(0);
+                    model1.getDataVector().remove(row);
+                }
+            };
+        Timer timer1 =new Timer(delay1,taskPerformer1);
+        timer1.start();
+        if (flag){
+            timer.stop();
+            timer1.stop();
+        }
+        }
+    }
+
